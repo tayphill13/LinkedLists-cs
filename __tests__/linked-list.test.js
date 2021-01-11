0f4d952f02a832f9fdd39e9b2faf9fecd36cae33
Taylor Phillips <taylorphillips133@gmail.com>
2021-01-11 13:47:58 -0800

    add test for adding a node to the end of the linked list, developing insert last method

--- __tests__/linked-list.test.js
+++ __tests__/linked-list.test.js
@@ -2,6 +2,19 @@ import LinkedList from '../src/linked-list.js';
 
 describe('LinkedList', () => {
 
+  let linkedListWithNodes = new LinkedList();
+
+  beforeEach(() => {
+    linkedListWithNodes.insertLast("node1");
+    linkedListWithNodes.insertLast("node2");
+    linkedListWithNodes.insertLast("node3");
+    linkedListWithNodes.insertLast("node4");
+  });
+
+  afterEach(() => {
+    linkedListWithNodes.head = null;
+  });
+
   test('it should construct a linked list with a head property', () => {
       let linkedList = new LinkedList();
       expect(linkedList.head).toEqual(null);
@@ -12,4 +25,9 @@ describe('LinkedList', () => {
     linkedList.insertLast("head");
     expect(linkedList.head.data).toEqual("head");
   })
+
+  test('LinkedList.prototype.insertLast() should add a node at the end of a linked list', () => {
+    linkedListWithNodes.insertLast("new last");
+    expect(linkedListWithNodes.head.next.next.next.next.data).toEqual("new last");
+  });
 });
